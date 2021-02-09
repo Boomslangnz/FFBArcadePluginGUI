43672f1bd7557a84048f5ea58e2a20b1f4aca24c
Aaron M <Boomslangnz@hotmail.com>
2021-02-09 17:39:19 +1300

    Add SWDC and MKGPUSA

--- Form1.h
+++ Form1.h
@@ -53,6 +53,7 @@ along with FFB Arcade Plugin GUI.If not, see < https://www.gnu.org/licenses/>.
 #include "MameSelect.h"
 #include "MKGP100.h"
 #include "MKGP110.h"
+#include "MKGPUSA.h"
 #include "Outrun2.h"
 #include "Outrun2Input.h"
 #include "PokkenTournament.h"
@@ -65,6 +66,7 @@ along with FFB Arcade Plugin GUI.If not, see < https://www.gnu.org/licenses/>.
 #include "StrengthInput.h"
 #include "StormRacerG.h"
 #include "SupermodelSelect.h"
+#include "SWDC2018.h"
 #include "WackyRaces.h"
 #include "WMMT5.h"
 #include "WMMT5Input.h"
@@ -511,6 +513,18 @@ namespace FFBPluginGUI {
 			StormRacerG^ obj1 = gcnew StormRacerG(this);
 			obj1->ShowDialog();
 		}
+		else if (GameSelect == 50)
+		{
+		this->Hide();
+		SWDC2018^ obj1 = gcnew SWDC2018(this);
+		obj1->ShowDialog();
+		}
+		else if (GameSelect == 51)
+		{
+		this->Hide();
+		MKGPUSA^ obj1 = gcnew MKGPUSA(this);
+		obj1->ShowDialog();
+		}
 		else
 		{
 			MessageBox::Show("FFBPlugin.ini does not contain GameId or does not exist");
@@ -528,7 +542,7 @@ namespace FFBPluginGUI {
 		else if (GameSelect == 2 || GameSelect == 4 || GameSelect == 5 || GameSelect == 6 || GameSelect == 7 || GameSelect == 8 || GameSelect == 10 || GameSelect == 11 || GameSelect == 13 || GameSelect == 14 || GameSelect == 15 || GameSelect == 16 ||
 			GameSelect == 17 || GameSelect == 18 || GameSelect == 19 || GameSelect == 20 || GameSelect == 21 || GameSelect == 22 || GameSelect == 23 || GameSelect == 24 || GameSelect == 27 || GameSelect == 30 || GameSelect == 31 || GameSelect == 32 || 
 			GameSelect == 33 || GameSelect == 34 || GameSelect == 36 || GameSelect == 37 || GameSelect == 38 || GameSelect == 39 || GameSelect == 40 || GameSelect == 41 || GameSelect == 42 || GameSelect == 43 || GameSelect == 44 || GameSelect == 45 || 
-			GameSelect == 46 || GameSelect == 47 || GameSelect == 48 || GameSelect == 49)
+			GameSelect == 46 || GameSelect == 47 || GameSelect == 48 || GameSelect == 49 || GameSelect == 50 || GameSelect == 51)
 		{
 			this->Hide();
 			StrengthInput^ obj1 = gcnew StrengthInput(this);
@@ -605,7 +619,7 @@ namespace FFBPluginGUI {
 		else if (GameSelect == 1 || GameSelect == 2 || GameSelect == 3 || GameSelect == 5 || GameSelect == 6 || GameSelect == 7 || GameSelect == 8 || GameSelect == 9 || GameSelect == 10 || GameSelect == 11 || GameSelect == 13 ||
 			GameSelect == 14 || GameSelect == 17 || GameSelect == 18 || GameSelect == 19 || GameSelect == 20 || GameSelect == 21 || GameSelect == 22 || GameSelect == 25 || GameSelect == 26 || GameSelect == 27 || GameSelect == 28 || 
 			GameSelect == 29 || GameSelect == 30 || GameSelect == 31 || GameSelect == 32 || GameSelect == 33 || GameSelect == 34 || GameSelect == 36 || GameSelect == 38 || GameSelect == 39 || GameSelect == 40 || 
-			GameSelect == 41 || GameSelect == 42 || GameSelect == 43 || GameSelect == 44 || GameSelect == 45 || GameSelect == 47 || GameSelect == 48 || GameSelect == 49)
+			GameSelect == 41 || GameSelect == 42 || GameSelect == 43 || GameSelect == 44 || GameSelect == 45 || GameSelect == 47 || GameSelect == 48 || GameSelect == 49 || GameSelect == 50 || GameSelect == 51)
 		{
 			this->Hide();
 			Help^ obj1 = gcnew Help(this);
