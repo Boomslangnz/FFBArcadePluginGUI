d644a5af2a6634b4489facc0eb0dfad77221d780
Boomslangnz <Boomslangnz@hotmail.com>
2023-03-18 15:01:08 +1300

    Gaelco FFB Changes

--- GaelcoTuningRace.h
+++ GaelcoTuningRace.h
@@ -37,6 +37,7 @@ namespace FFBPluginGUI {
 			this->AutoAddComponent("CommonOptions");
 			this->AutoAddComponent("AlternativeFFB");
 			this->AutoAddComponent("EnableDamper");
+			this->AutoAddComponent("DoubleConstant");
 
 			this->AutoAddLongTrackBarBlock("DamperStrength", L"Damper Strength", 0, 100, L"Strength of damper effect to lower oscillation");
 
